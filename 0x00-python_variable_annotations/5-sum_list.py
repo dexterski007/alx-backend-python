738b407a2f4e5485ef02365910f798545d5dfcff
dexterski007 <mbouj007@gmail.com>
2024-08-01 13:16:16 +0100

    fix task5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import typing
 """ list of float => sum float """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """  takes a list input_list of floats as argument
     and returns their sum as a float """
     return float(sum(input_list))
