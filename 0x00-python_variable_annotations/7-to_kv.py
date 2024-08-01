50f2e7b023eab68ad7d5dbc20b0fbfc1fa105f45
dexterski007 <mbouj007@gmail.com>
2024-08-01 13:40:20 +0100

    uninon

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ list of mixed => sum float """
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ takes a string k and an int OR
     float v as arguments and returns a tuple """
     new_tuple = (k, float(v ** 2))
