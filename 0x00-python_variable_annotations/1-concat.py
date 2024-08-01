bbf41705e907ca67932e53bd6fab482c1f97f02f
dexterski007 <mbouj007@gmail.com>
2024-08-01 12:19:22 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" string concatenante"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ takes 2 args and return their concatenated strings """
+    return str1 + str2
