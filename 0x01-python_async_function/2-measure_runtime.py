8a9fb385199598f52b81e9dbe97ba60aac87a879
dexterski007 <mbouj007@gmail.com>
2024-08-06 00:04:33 +0100

    task 2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-""" execute multiple coroutines at the same time with async """
+""" measuring time """
 import asyncio
-from typing import List
-import random
-
-
+import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ wait for randomness"""
-    delays = []
-    for i in range(n):
-        delay = await wait_random(max_delay)
-        delays.append(delay)
-    return delays
+def measure_time(n: int, max_delay: int) -> float:
+    """ measure time """
+    begin = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    total_time = end - begin
+    return total_time / n
