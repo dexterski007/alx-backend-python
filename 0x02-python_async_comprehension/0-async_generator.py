e6a131a1aa31c65a0be96f47c00f1223eec3c534
dexterski007 <mbouj007@gmail.com>
2024-08-06 20:58:17 +0100

    what can you do

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ coroutines task"""
-from typing import AsyncGenerator
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ wait for randomness"""
     for _ in range(10):
         await asyncio.sleep(1)
