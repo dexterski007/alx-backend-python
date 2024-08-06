2d326ebeef94fe946df9cde45406dcfe98b6b41f
dexterski007 <mbouj007@gmail.com>
2024-08-06 21:54:22 +0100

    pycodestyle

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ task 1 of coroutines """
 from typing import List
-
 async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """ async comprehensions """
