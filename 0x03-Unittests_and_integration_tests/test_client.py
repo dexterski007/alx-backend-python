8e1703656c6da6111d6021d280e84899fc038035
dexterski007 <mbouj007@gmail.com>
2024-08-24 12:46:19 +0100

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@ from client import GithubOrgClient
 from unittest.mock import Mock, patch, PropertyMock
 import unittest
 from parameterized import parameterized
+from typing import Dict, Any
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -40,6 +41,16 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(git_cli.public_repos(), ["git_repo"])
             mock_json.assert_called_once()
             mocked.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo: Dict,
+                         license_key: str, result) -> None:
+        """ testing the presence of license """
+        github_cli = GithubOrgClient("microsoft")
+        self.assertEqual(github_cli.has_license(repo,license_key), result)
 
 
 if __name__ == '__main__':
